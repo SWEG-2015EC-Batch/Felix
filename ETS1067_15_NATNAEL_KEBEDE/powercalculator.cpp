c628430032bddce5be597b801be9d046d8641e23
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-11-14 14:17:19 -0500

    Create powercalculator.cpp

---
+++ ETS1067_15_NATNAEL_KEBEDE/powercalculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    double x;
+    cout << "x = ";
+    cin >>x;
+   double y;
+    cout << "y = ";
+    cin >>y;
+    double result;
+    result = pow(x,y);
+    cout << "x the power of y = " << result <<endl;
+    return 0;
+
+ }
