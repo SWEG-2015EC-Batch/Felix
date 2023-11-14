d1965ccff4bc2ef964569c6b3b9383a128634281
Natnael Samson <110995950+Nattyy-1@users.noreply.github.com>
2023-11-14 20:42:39 +0300

    Create powerCalc.cpp

---
+++ ETS1065_15_Natnael_Samson/powerCalc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x, y, result;
+    cout << "Enter the value of x: ";
+    cin >> x;
+    cout << "Enter the value of y: ";
+    cin >> y;
+
+    // Calculate x to the power of y
+    result = pow(x, y);
+    cout << x << " raised to the power of " << y << " is " << result << endl;
+
+    return 0;
+}
