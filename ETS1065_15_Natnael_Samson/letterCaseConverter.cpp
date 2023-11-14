d639a76adf2ecdff9d714fcf0789f8b0b19f0808
Natnael Samson <110995950+Nattyy-1@users.noreply.github.com>
2023-11-14 21:37:46 +0300

    Update letterCaseConverter.cpp

--- ETS1065_15_Natnael_Samson/letterCaseConverter.cpp
+++ ETS1065_15_Natnael_Samson/letterCaseConverter.cpp
@@ -1,19 +1,17 @@
+//This program takes a user-inputted letter and converts it to its uppercase or lowercase equivalent, or displays an error message if the input is not a letter.
 #include <iostream>
 #include <ctype.h>
 using namespace std;
-
 int main() {
     char letter;
     cout << "Enter a letter: ";
     cin >> letter;
-
-    if (islower(letter)) {
+    if (islower(letter)) {
         cout << "Uppercase equivalent: " << (char)toupper(letter) << endl;
     } else if (isupper(letter)) {
         cout << "Lowercase equivalent: " << (char)tolower(letter) << endl;
     } else {
         cout << "Invalid input. Please enter a letter." << endl;
     }
-
-    return 0;
+    return 0;
 }
