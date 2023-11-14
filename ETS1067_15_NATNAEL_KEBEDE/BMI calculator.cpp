b4db536f6c39f71146208cdd8c490c0f4c5f5004
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-11-14 14:10:31 -0500

    Create BMI calculator.cpp

---
+++ ETS1067_15_NATNAEL_KEBEDE/BMI calculator.cpp
@@ -0,0 +1,16 @@
+//this program calculate your body mass index
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout << "Enter your Height in meter " << endl;
+    double Height;
+    cin >>Height;
+    cout << "Enter your Weight in kilogram " << endl;
+    double Weight;
+    cin >>Weight;
+    double BMI;
+    BMI = Weight/(Height*Height);
+    cout << "Your BMI(body mass index) = " <<BMI<< endl;
+    return 0;
+}
