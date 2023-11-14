d8e6154f7e106e91e10daeb141d1f1d91633a0e7
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-11-14 14:12:13 -0500

    Create letter case converter.cpp

---
+++ ETS1067_15_NATNAEL_KEBEDE/letter case converter.cpp
@@ -0,0 +1,20 @@
+//this program converts letter cases
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+
+int main() {
+ char letter;
+ cout << "Enter a letter: ";
+ cin >> letter;
+
+ if (islower(letter)) {
+cout << "Uppercase equivalent: " << (char)toupper(letter) << endl;
+ } else if (isupper(letter)) {
+ cout << "Lowercase equivalent: " << (char)tolower(letter) << endl;
+ } else {
+ cout << "Invalid input. Please enter a letter." << endl;
+ }
+
+return 0;
+}
