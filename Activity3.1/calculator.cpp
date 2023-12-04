e1aa4e8a9fcfcb757a0015aab420bccf388c5d94
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-04 02:56:32 -0500

    Create calculator.cpp

---
+++ Activity3.1/calculator.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    double num1,num2,insert;
+    char Op;
+    do
+    {
+    cout << "Enter the operation:"<<endl;
+    cout << "----------------------------------------"<<endl;
+    cin>> num1>>Op>>num2;
+    switch (Op)
+    {
+    case '+':
+          cout <<"result = "<< num1 + num2<<endl;
+        break;
+    case '-':
+        cout <<"result = "<< num1 - num2<<endl;
+        break;
+    case '/':
+        if (num2 != 0)
+        {
+          cout <<"result = "<<num1 / num2<<endl;
+        }
+        else{
+            cout <<"error"<<endl;
+        }
+         break;
+    case '*':
+         cout <<"result = "<< num1 * num2<<endl;
+         break;
+    case '%':
+          cout <<"result = "<<static_cast<long>(num1) % static_cast<long>(num2)<<endl;
+          break;
+    default:
+        cout <<"you insert the wrong operator"<<endl;
+        break;
+
+    }
+    cout << "----------------------------------------"<<endl;
+    cout << "Enter any number other than 0 to continue:- ";
+    cin>>insert;
+    ;
+    } while (insert != 0);
+    return 0;
+}
