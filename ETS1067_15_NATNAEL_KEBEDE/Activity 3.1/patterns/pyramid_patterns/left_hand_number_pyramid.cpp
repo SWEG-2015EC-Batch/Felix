906cf2f8b72491e0c3e3586fcceab3e392252824
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-17 08:42:12 -0500

    Update left_hand_number_pyramid.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/left_hand_number_pyramid.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/left_hand_number_pyramid.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int row;
+    cout << "Enter the number of rows: ";
+    cin >> row;
+
+    for (int i = 1; i <= row; i++) {
+        int value = 1; // Initialize the value to print
+        for (int j = 1; j <= i; j++) {
+            cout << value << " ";
+            value++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
