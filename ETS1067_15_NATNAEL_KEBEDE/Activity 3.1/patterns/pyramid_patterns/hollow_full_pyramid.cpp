0aa9bbfd9e12fcc56955f74ee6947ce9f36ec442
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-17 07:44:37 -0500

    Update hollow_full_pyramid.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/hollow_full_pyramid.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/hollow_full_pyramid.cpp
@@ -1,1 +1,18 @@
-
+// Hollow full Pyramid
+#include <iostream>
+using namespace std;
+int main(){
+                cout << "Please enter the size of the hollow pyramid: ";
+                cin >> size;
+                for (int i = {0}; i < size; i++) {
+                    for (int j = {0}; j < 2 * size - 1; j++) {
+                        if (j == size - i - 1 || j == size + i - 1 || i == size - 1) {
+                            cout << "* ";
+                        } else {
+                            cout << "  ";
+                        }
+                    }
+                    cout << endl;
+                }
+                return 0;
+}
