278f5ee566ae138e09423c075900c04022037986
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-17 07:56:27 -0500

    Update 1-5_rectangle.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/rectangle_patterns/1-5_rectangle.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/rectangle_patterns/1-5_rectangle.cpp
@@ -1,1 +1,14 @@
-
+//1-5 matrix
+#include <iostream>
+usong namespace std;
+int main(){
+                for (int i {0}; i < 5; i++) {
+                    for (int j {0}; j < 5; j++) {
+                        cout << num_matrix <<" ";
+                        num_matrix++;
+                    }
+                    num_matrix = 1;
+                    cout << endl;
+                }
+               return 0;
+}
