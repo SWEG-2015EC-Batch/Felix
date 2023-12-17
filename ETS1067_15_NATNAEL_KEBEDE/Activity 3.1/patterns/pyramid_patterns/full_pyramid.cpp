c70343e59bb0135fac9bcabfec864e1b8f4e257c
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-17 07:41:54 -0500

    Update full_pyramid.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/full_pyramid.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/full_pyramid.cpp
@@ -1,1 +1,19 @@
-
+//full_Pyramid
+#include <iostream>
+using namespace std;
+int main()
+{
+                cout << "Please enter the size of the pyramid: ";
+                cin >> size;
+                for (int i {0}; i < size; i++) {
+                    for (int j {0}; j < 2 * size - 1; j++) {
+                        if (j >= size - i - 1 && j <= size + i - 1) {
+                            cout << "* ";
+                        } else {
+                            cout << "  ";
+                        }
+                    }
+                    cout << endl;
+                }
+                return 0;
+}
