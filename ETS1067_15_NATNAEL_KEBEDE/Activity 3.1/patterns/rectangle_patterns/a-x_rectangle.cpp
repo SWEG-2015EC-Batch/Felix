ec5cc6d82b75dc05f5d5b4442494377813385cb7
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-17 08:05:46 -0500

    Update a-x_rectangle.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/rectangle_patterns/a-x_rectangle.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/rectangle_patterns/a-x_rectangle.cpp
@@ -1,1 +1,15 @@
-
+//'a' to 'x' rectangle
+#include <iostream>
+using namespace std;
+int main(){
+                for (int i {0}; i < 4; i++) {
+                    for (int j {0}; j < 6; j++) {
+                        if (letter <= 'x'){
+                           cout << letter << " ";
+                           letter++; 
+                        }
+                    }
+                    cout <<endl;
+                }
+                return 0;
+}
