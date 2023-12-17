ba3d3488b03af8c202dbd96d87b1f1a08fcefbfa
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-17 04:27:35 -0500

    Update hollow_rectangle.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/rectangle_patterns/hollow_rectangle.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/rectangle_patterns/hollow_rectangle.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int width, height;
 
+   cout << "Enter the width of the rectangle: ";
+    cin >> width;
+
+   cout << "Enter the height of the rectangle: ";
+   cin >> height;
+
+    if (width > 0 && height > 0) {
+        cout << "Hollow Rectangle of size " << width << "x" << height << ":" << std::endl;
+        
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
+                    cout << "* ";
+                else
+                   cout << "  ";
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "Width and height should be greater than zero." << std::endl;
+    }
+
+    return 0;
+}
