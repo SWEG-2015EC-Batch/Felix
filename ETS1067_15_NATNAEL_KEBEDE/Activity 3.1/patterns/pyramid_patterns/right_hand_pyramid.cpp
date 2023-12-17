06e9dc7828cd1bddd99bf6109b9abcebacf6bd73
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-17 06:56:56 -0500

    Update right_hand_pyramid.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/right_hand_pyramid.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/right_hand_pyramid.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int row;
+
+    cout<<"Enter a number: ";
+    cin>>row;
+
+    for(int i = 1; i <= row; i++){
+        for(int j = row; j >= 1 ; j--){
+            if(j <= i){
+                cout<<j<<" ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
