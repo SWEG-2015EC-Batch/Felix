c5bd640a20dce68577a0d3fc205cd22be35b3138
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-17 08:24:01 -0500

    Update letter_pyramid.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/letter_pyramid.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/letter_pyramid.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int numRows;
 
+    cout << "Enter the number of rows for the pattern: ";
+    cin >> numRows;
+
+    if (numRows > 0) {
+        char ch = 'A';
+
+        cout << "Right Angle Triangle Pattern:" << endl;
+        for (int i = 1; i <= numRows; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                cout << ch << " ";
+                ch++;
+            }
+            ch = 'A'; // Reset the character to 'A' for the next row
+          cout << endl;
+        }
+    } else {
+       cout << "Number of rows should be greater than zero." << endl;
+    }
+
+    return 0;
+}
