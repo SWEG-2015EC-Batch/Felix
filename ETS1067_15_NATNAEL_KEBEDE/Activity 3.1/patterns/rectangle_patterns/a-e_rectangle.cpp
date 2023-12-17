653fd3a06e60b86d6acb209ec3e0be4d17460162
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-17 08:00:51 -0500

    Update a-e_rectangle.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/rectangle_patterns/a-e_rectangle.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/rectangle_patterns/a-e_rectangle.cpp
@@ -1,1 +1,14 @@
-
+ // 'a' to 'e' rectangle
+#include <iostream>
+using namespace std;
+int main(){
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        cout << letter << " ";
+                        letter++;
+                    }
+                    letter = 'a';
+                    cout << endl;
+                }
+                return 0;
+}
