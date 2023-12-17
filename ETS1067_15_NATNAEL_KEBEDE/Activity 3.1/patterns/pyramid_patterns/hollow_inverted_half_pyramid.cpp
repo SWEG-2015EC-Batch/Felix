c363f1b082a06cf6a71bc31d76719d5d8133e26b
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-17 07:34:54 -0500

    Update hollow_inverted_half_pyramid.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/hollow_inverted_half_pyramid.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/pyramid_patterns/hollow_inverted_half_pyramid.cpp
@@ -1,1 +1,19 @@
-
+// Inverted Hollow Right Angled Triangle Pattern
+#include <iostream>
+using namespace std;
+int main(){
+                cout << "Please enter the size of the pattern you want: ";
+                cin >> size;
+                for (int i = 0; i < size; i++) {
+                    for (int j = size; j > i; j--) {
+                        if (i == 0 || j == size || i == size - 1 || j == i + 1) {
+                            cout << "*";
+                        } else {
+                            cout << " ";
+                        }
+                        cout << " ";
+                    }
+                    cout << endl;
+                }
+                return 0;
+}
