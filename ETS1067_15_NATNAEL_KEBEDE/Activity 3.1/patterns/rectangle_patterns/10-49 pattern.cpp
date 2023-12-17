f9485c6166392395f191642c02d7b7e774cb9575
Natnael kebede <149377944+Natikebe45@users.noreply.github.com>
2023-12-16 21:29:32 -0500

    Update 10-49 pattern.cpp

--- ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/rectangle_patterns/10-49 pattern.cpp
+++ ETS1067_15_NATNAEL_KEBEDE/Activity 3.1/patterns/rectangle_patterns/10-49 pattern.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int count = 10;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            cout << count << "\t";
+            count++;
+        }
+      cout << endl;
+    }
+    return 0;
+}
