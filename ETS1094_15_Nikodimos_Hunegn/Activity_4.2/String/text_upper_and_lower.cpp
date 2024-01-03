7b27dd7f3f382fa3eb7845dfc7145645d6cbe647
Nikodimos Hunegn <90319480+Nikohacks@users.noreply.github.com>
2024-01-02 17:31:27 -0700

    Add files via upload

---
+++ ETS1094_15_Nikodimos_Hunegn/Activity_4.2/String/text_upper_and_lower.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main(){
+
+    string text = "";
+
+    cout<<"Enter the text: ";
+    getline(cin, text);
+
+    for(int i = 0; i < text.size(); i++){
+        text[i] = toupper(text[i]);
+    }
+
+    cout<<"Uppercase: "<<text<<endl;
+    
+    for(int i = 0; i < text.size(); i++){
+        text[i] = tolower(text[i]);
+    }
+
+    cout<<"Lowercase: "<<text;
+
+}
