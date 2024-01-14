9802ea6781ff9b33f4ae15e36a419004b3468ed5
Natnael Samson <110995950+Nattyy-1@users.noreply.github.com>
2024-01-14 19:24:18 +0300

    Create Question_5.cpp

---
+++ ETS1065_15_Natnael_Samson/Activity_4.2/Array_2D/Question_5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int size = 4;
+    char friends[size][size] = {
+        {'*', ' ', ' ', '*'},
+        {' ', '*', '*', ' '},
+        {'*', ' ', ' ', '*'},
+        {'*', '*', ' ', '*'}
+    };
+
+    // Display the matrix
+    cout << "   ";
+    for (int i = 0; i < size; i++) {
+        cout << i << "  ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < size; i++) {
+        cout << i << "  ";
+        for (int j = 0; j < size; j++) {
+            cout << friends[i][j] << "  ";
+        }
+        cout << endl;
+    }
+    int friendPairs = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (friends[i][j] == '*' && friends[j][i] == '*') {
+                friendPairs++;
+            }
+        }
+    }
+
+    cout << "Number of friend pairs: " << friendPairs << endl;
+
+    return 0;
+}
